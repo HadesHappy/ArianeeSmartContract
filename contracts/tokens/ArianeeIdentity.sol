81580909b2282ff9bd2064b4cd7ed6258e05503e
Maxime <maxime@vaugam.com>
2019-04-17 10:24:13 +0200

    add identity in bytes4

--- contracts/tokens/ArianeeIdentity.sol
+++ contracts/tokens/ArianeeIdentity.sol
@@ -47,10 +47,35 @@ Ownable
   mapping(address => uint256) public compromiseDate;
 
   /**
-   * 
+   * @dev Mapping from addressId to address.
    */
-   address[] public addressListing;
-
+   mapping(bytes4=>address) public addressListing;
+   
+   /**
+    * @dev This emits when a new address is approved.
+    */
+   event AddressApprovedAdded(address _newIdentity, bytes4 _addressId);
+   
+   /**
+    * @dev This emits when an address is removed from approvedList.
+    */
+   event AddressApprovedRemoved(address _newIdentity);
+   
+   /**
+    * @dev This emits when a new address is approved.
+    */
+   event URIUpdated(address _identity, string _uri, bytes32 _imprint);
+   
+   /**
+    * @dev This emits when an identity change its URI and Imprint.
+    */
+   event URIValidate(address _identity, string _uri, bytes32 _imprint);
+   
+   /**
+    * @dev This emits when an identity change is validated by the contract owner.
+    */
+   event IdentityCompromised(address _identity, uint256 _compromiseDate);
+   
   constructor() public{
     name = "Arianee Identity";
     symbol = "AriaI";
@@ -64,17 +89,47 @@ Ownable
     require(approvedList[_identity]);
     _;
   }
+
+
+  function _getSlice(uint256 begin, uint256 end, bytes memory text) public pure returns (bytes memory) {
+    bytes memory a = new bytes(end-begin+1);
+    for(uint i=0;i<=end-begin;i++){
+        a[i] = bytes(text)[i+begin-1];
+    }
+    return bytes(a);    
+  }
+
+  function convertBytesToBytes4(bytes memory inBytes) internal pure returns (bytes4 outBytes4) {
+    if (inBytes.length == 0) {
+        return 0x0;
+    }
+
+    assembly {
+        outBytes4 := mload(add(inBytes, 32))
+    }
+  }
+  
+  
   /**
    * @dev Add a new address to approvedList
    * @notice Can only be called by the owner, allow an address to create/update his URI and Imprint.
    * @param _newIdentity Address to authorize.
+   * @return Id for address in bytes4.
    */
-  function addAddressToApprovedList(address _newIdentity) public onlyOwner() returns (uint256){
+  function addAddressToApprovedList(address _newIdentity) public onlyOwner() returns (bytes4){
     approvedList[_newIdentity] = true;
-    uint256 _addressId = addressListing.push(_newIdentity);
+    
+    bytes memory _bytesAddress = abi.encodePacked(_newIdentity);
+    bytes memory _addressIdDyn = _getSlice(1,6,_bytesAddress);
+    bytes4 _addressId = convertBytesToBytes4(_addressIdDyn);
+    
+    addressListing[_addressId] = _newIdentity;
+    
+    emit AddressApprovedAdded(_newIdentity, _addressId);
+    
     return _addressId;
   }
-  
+
  /**
   * @dev remove an address from approvedList.
   * @notice Can only be called by the owner.
