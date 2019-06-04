41579f667830e78e4fc14e4f8a575a78849509a3
Maxime <maxime@vaugam.com>
2019-06-04 11:05:44 +0200

    respect eslint spec

--- test/helpers/exceptions.js
+++ test/helpers/exceptions.js
@@ -1,4 +1,4 @@
-const PREFIX = "Returned error: VM Exception while processing transaction: ";
+const PREFIX = 'Returned error: VM Exception while processing transaction: ';
 
 async function tryCatch(promise, message) {
   try {
@@ -6,17 +6,17 @@ async function tryCatch(promise, message) {
     throw null;
   }
   catch (error) {
-    assert(error, "Expected an error but did not get one");
-    assert(error.message.startsWith(PREFIX + message), "Expected an error starting with '" + PREFIX + message + "' but got '" + error.message + "' instead");
+    assert(error, 'Expected an error but did not get one');
+    assert(error.message.startsWith(PREFIX + message), 'Expected an error starting with "' + PREFIX + message + '" but got "' + error.message + '" instead');
   }
 };
 
 module.exports = {
-  catchRevert            : async function(promise) {await tryCatch(promise, "revert"             );},
-  catchOutOfGas          : async function(promise) {await tryCatch(promise, "out of gas"         );},
-  catchInvalidJump       : async function(promise) {await tryCatch(promise, "invalid JUMP"       );},
-  catchInvalidOpcode     : async function(promise) {await tryCatch(promise, "invalid opcode"     );},
-  catchStackOverflow     : async function(promise) {await tryCatch(promise, "stack overflow"     );},
-  catchStackUnderflow    : async function(promise) {await tryCatch(promise, "stack underflow"    );},
-  catchStaticStateChange : async function(promise) {await tryCatch(promise, "static state change");},
+  catchRevert            : async function(promise) {await tryCatch(promise, 'revert'             );},
+  catchOutOfGas          : async function(promise) {await tryCatch(promise, 'out of gas'         );},
+  catchInvalidJump       : async function(promise) {await tryCatch(promise, 'invalid JUMP'       );},
+  catchInvalidOpcode     : async function(promise) {await tryCatch(promise, 'invalid opcode'     );},
+  catchStackOverflow     : async function(promise) {await tryCatch(promise, 'stack overflow'     );},
+  catchStackUnderflow    : async function(promise) {await tryCatch(promise, 'stack underflow'    );},
+  catchStaticStateChange : async function(promise) {await tryCatch(promise, 'static state change');},
 };
--- truffle.js
+++ truffle.js
@@ -1,16 +1,16 @@
 module.exports = {
-    networks: {
-        test: {
-            host: '127.0.0.1',
-            port: 8546,
-            gas: 6721975,
-            network_id: "*",
-            from: '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1'
-        }
-    },
-    compilers: {
+  networks: {
+    test: {
+      host: '127.0.0.1',
+      port: 8546,
+      gas: 6721975,
+      network_id: '*',
+      from: '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1'
+    }
+  },
+  compilers: {
     solc: {
-      version: "0.5.1"
-	  }
-	}
+      version: '0.5.1'
+    }
+  }
 };
